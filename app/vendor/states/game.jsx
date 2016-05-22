b956071c0f5b7abab5d07e2bf6ce67bf5c659191
Goodzilla <ronan.cotillard@gmail.com>
2016-05-22 16:37:30 +0200

    Snake rotation and debug text

--- app/vendor/states/game.jsx
+++ app/vendor/states/game.jsx
@@ -13,10 +13,16 @@ PhaserBp.Game.prototype = {
 		this.numSnakeSections = 10;
 		this.snakeSpacer = 5;
 		this.velocity = 300;
+		this.currentAngle = 0;
+		this.maxTurningAngle = 4;
 
 		// WORLD
 		this.world.setBounds(0, 0, worldWidth, worldHeight);
 		this.add.tileSprite(0, 0, worldWidth, worldHeight, 'background');
+
+		// FPS COUNTER
+		this.time.advancedTiming = true;
+		this.time.desiredFps = 60;
 
 		// ACTIVATE PHYSICS
 		this.physics.startSystem(Phaser.Physics.ARCADE);
@@ -44,10 +50,27 @@ PhaserBp.Game.prototype = {
     },
 
     update: function() {
-		// HEAD ANGLE AND MOVEMENT
-		var newAngle = this.physics.arcade.angleToPointer(this.snakeHead);
-		this.snakeHead.rotation = newAngle;
-		this.physics.arcade.velocityFromRotation(newAngle, this.velocity, this.snakeHead.body.velocity);
+		// HEAD ANGLE
+		var newRotation = this.math.normalizeAngle(this.physics.arcade.angleToPointer(this.snakeHead));
+		this.snakeHead.rotation = newRotation;
+
+		// LIMIT SNAKE ROTATION
+		this.targetAngle = this.math.radToDeg(newRotation);
+
+		if (this.currentAngle - this.targetAngle !== 0) {
+			if (Math.abs(this.currentAngle - this.targetAngle) < 180) {
+				// ROTATE DIRECTLY TOWARDS THE TARGET
+				if (this.currentAngle < this.targetAngle) this.currentAngle += this.maxTurningAngle;
+				else this.currentAngle -= this.maxTurningAngle;
+			} else {
+				// ROTATE IN THE REVERSE DIRECTION
+				if (this.currentAngle < this.targetAngle) this.currentAngle -= this.maxTurningAngle;
+				else this.currentAngle += this.maxTurningAngle;
+			}
+			this.currentAngle = ((this.currentAngle % 360) + 360) % 360;
+
+			this.physics.arcade.velocityFromAngle(this.currentAngle, this.velocity, this.snakeHead.body.velocity);
+		}
 
 		// BODY MOVEMENT AND ANGLE
 	    var part = this.snakePath.pop();
@@ -66,5 +89,12 @@ PhaserBp.Game.prototype = {
 				this.snakeSection[i].rotation = this.physics.arcade.angleBetween(this.snakeSection[i], this.snakeSection[i-1]);
 			}
 	    }
+	},
+
+	render: function() {
+		this.game.debug.spriteInfo(this.snakeHead, 32, 32);
+		this.game.debug.text(this.time.fps, 2, 14, "#00ff00");
+		this.game.debug.text('currentAngle: ' + this.currentAngle, 32, 150);
+		this.game.debug.text('targetAngle: ' + this.targetAngle, 32, 180);
 	}
 };
